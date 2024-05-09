bcdbedacc2b6556a121afbaa8f5ef875d445e3ff
WASSIMBESSAIS <wassimbessaise11@gmail.com>
2024-05-09 12:18:09 +0100

    add next-auth and modify the header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,25 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "BRUDAGRAM",
-  description: "A clone of instagram built with Next.js and Tailwind CSS",
+  title: "Instagram Clone",
+  description: "A clone of Instagram built with Next.js and Tailwind CSS",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
 
-        {children}
-      </body>
-    </html>
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
